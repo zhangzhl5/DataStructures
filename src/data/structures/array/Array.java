1744f1544a0d3a2bbf0d51701127b6720e372aa9
zhangzhl <185378697@qq.com>
2020-03-15 18:55:18 +0800

    封装数组类
    Change-Id: Ic485c228898251f409097fabafb2da8a25e8555f
    Signed-off-by: zhangzhl <185378697@qq.com>

---
+++ src/data/structures/array/Array.java
@@ -0,0 +1,48 @@
+package data.structures.array;
+
+public class Array {
+
+    private int[] data;
+    private int size;
+
+    // 构造函数，传入数组的容量capacity构造Array
+    public Array(int capacity){
+        data = new int[capacity];
+        size = 0;
+    }
+
+    // 无参数的构造函数，默认数组的容量capacity=10
+    public Array(){
+        this(10);
+    }
+
+    // 获取数组的容量
+    public int getCapacity(){
+        return data.length;
+    }
+
+    // 获取数组中的元素个数
+    public int getSize(){
+        return size;
+    }
+
+    // 返回数组是否为空
+    public boolean isEmpty(){
+        return size == 0;
+    }
+    
+    // 在index索引的位置插入一个新元素e
+    public void add(int index, int e){
+
+        if(size == data.length)
+            throw new IllegalArgumentException("Add failed. Array is full.");
+
+        if(index < 0 || index > size)
+            throw new IllegalArgumentException("Add failed. Require index >= 0 and index <= size.");
+
+        for(int i = size - 1; i >= index ; i --)
+            data[i + 1] = data[i];
+        data[index] = e;
+        size ++;
+    }
+}
