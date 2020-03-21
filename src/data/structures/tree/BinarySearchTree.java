a81731fc1049f439f2254cab47da6cd7de0c140b
zhangzhl <185378697@qq.com>
2020-03-21 13:55:20 +0800

    树的遍历
    Change-Id: I6bbf0b99c10ed5e8c56558ebee9ef74cefad611c
    Signed-off-by: zhangzhl <185378697@qq.com>

--- src/data/structures/tree/BinarySearchTree.java
+++ src/data/structures/tree/BinarySearchTree.java
@@ -58,5 +58,105 @@ public class BinarySearchTree<E extends Comparable<E>> {
 		}
 		return node;
 	}
+	
+	/**
+	 * 方法描述：判断二分搜索树中是否包含e
+	 * @param e
+	 * @return
+	 */
+	public boolean contains(E e) {
+		
+		return contains(root,e);
+	}
+	
+	/**
+	 * 方法描述：以root为根判断是否包含某元素e，递归算法
+	 * @param node
+	 * @param e
+	 * @return
+	 */
+	public boolean contains(Node node,E e) {
+		if(node == null) 
+			return false;
+		if(e.compareTo(node.e) == 0) {
+			return true;
+		} else if(e.compareTo(node.e) < 0) {
+			return contains(node.left,e);
+		} else {
+			return contains(node.right,e);
+		}
+	}
+	
+	/**
+	 * 方法描述：二分搜索树的前序遍历
+	 */
+	public void preOrder() {
+		preOrder(root);
+	}
 
+	/**
+	 * 方法描述：前序遍历以node为根的二分搜索树，递归遍历
+	 * @param node
+	 */
+	private void preOrder(Node node) {
+		if(node == null) {
+			return;
+		}
+		System.out.println(node.e);
+		preOrder(node.left);
+		preOrder(node.right);
+	}
+	
+	/**
+	 * 方法描述：二分搜索树的中序遍历
+	 */
+	public void inOrder() {
+		inOrder(root);
+	}
+
+	/**
+	 * 方法描述：中序遍历以node为根的二分搜索树，递归遍历
+	 * @param node
+	 */
+	private void inOrder(Node node) {
+		if(node == null) {
+			return;
+		}
+		preOrder(node.left);
+		System.out.println(node.e);
+		preOrder(node.right);
+	}
+	
+	/**
+	 * 方法描述：二分搜索树的后序遍历
+	 */
+	public void postOrder() {
+		postOrder(root);
+	}
+
+	/**
+	 * 方法描述：后序遍历以node为根的二分搜索树，递归遍历
+	 * @param node
+	 */
+	private void postOrder(Node node) {
+		if(node == null) {
+			return;
+		}
+		preOrder(node.left);
+		preOrder(node.right);
+		System.out.println(node.e);
+	}
+	
+	public static void main(String[] args) {
+		BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+		int[] nums = {5,3,6,8,4,2};
+		for(int s : nums) {
+			bst.add(s);
+		}
+		bst.preOrder();
+		System.out.println("---------");
+		bst.inOrder();
+		System.out.println("---------");
+		bst.postOrder();
+	}
 }
