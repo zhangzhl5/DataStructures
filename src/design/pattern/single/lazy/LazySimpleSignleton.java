f4ac42948dfeae427a1ddd1c0fa997c3a95d27e4
zhangzhl <185378697@qq.com>
2020-04-16 22:45:34 +0800

    懒汉式单例模式
    Change-Id: Ib9d5cf5187b5030f640a692ab5549352e2f3badf
    Signed-off-by: zhangzhl <185378697@qq.com>

---
+++ src/design/pattern/single/lazy/LazySimpleSignleton.java
@@ -0,0 +1,24 @@
+package design.pattern.single.lazy;
+
+/**
+ * 懒汉式单例模式
+ * 优点：节省内存消耗
+ * 缺点：线程不安全
+ * 
+ * @author zhangzhl
+ *
+ */
+public class LazySimpleSignleton {
+	
+	private LazySimpleSignleton() {};
+	
+	private static LazySimpleSignleton lazySimpleSignleton;
+	
+	public static LazySimpleSignleton getInstance() {
+		if(lazySimpleSignleton == null ) {
+			lazySimpleSignleton = new LazySimpleSignleton();
+		} 
+		return lazySimpleSignleton;
+	}
+
+}
