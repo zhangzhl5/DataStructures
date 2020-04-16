8972200e3f3bffaa9230e60cba0116d487f99e19
zhangzhl <185378697@qq.com>
2020-04-16 23:55:03 +0800

    添加常量
    Change-Id: Icb447e7ea197d6f98c5ad6a41c9ba8a291a48d23
    Signed-off-by: zhangzhl <185378697@qq.com>

--- src/design/pattern/single/register/EnumSignleton.java
+++ src/design/pattern/single/register/EnumSignleton.java
@@ -1,14 +1,25 @@
 package design.pattern.single.register;
 
 /**
- *懒汉式单例模式
- *枚举实现  
+ * 懒汉式单例模式 枚举实现  
+ * 
  * @author zhangzhl
  *
  */
 public enum EnumSignleton {
 	INATANCE;
-	
+
+	// 常量公用
+	private Object data;
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+
 	public static EnumSignleton getInstance() {
 		return INATANCE;
 	}
