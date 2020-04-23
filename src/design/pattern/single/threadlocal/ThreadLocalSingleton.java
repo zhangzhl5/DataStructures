8bf224597e387da0a0f76f49aaf6abb1a8cd4b0f
zhangzhl <185378697@qq.com>
2020-04-23 23:35:31 +0800

    单例
    Change-Id: I7b3c14bffae256f46191724ff51aec4c5d1c6936
    Signed-off-by: zhangzhl <185378697@qq.com>

--- src/design/pattern/single/threadlocal/ThreadLocalSingleton.java
+++ src/design/pattern/single/threadlocal/ThreadLocalSingleton.java
@@ -1,17 +1,22 @@
 package design.pattern.single.threadlocal;
 
+/**
+ * 
+ * @author zhangzhl
+ *
+ */
 public class ThreadLocalSingleton {
-    private static final ThreadLocal<ThreadLocalSingleton> threadLocalInstance =
-            new ThreadLocal<ThreadLocalSingleton>(){
-                @Override
-                protected ThreadLocalSingleton initialValue() {
-                    return new ThreadLocalSingleton();
-                }
-            };
+	
+	private static final ThreadLocal<ThreadLocalSingleton> threadLocalInstance = new ThreadLocal<ThreadLocalSingleton>() {
+		@Override
+		protected ThreadLocalSingleton initialValue() {
+			return new ThreadLocalSingleton();
+		}
+	};
 
-    private ThreadLocalSingleton(){}
+	private ThreadLocalSingleton() {}
 
-    public static ThreadLocalSingleton getInstance(){
-        return threadLocalInstance.get();
-    }
+	public static ThreadLocalSingleton getInstance() {
+		return threadLocalInstance.get();
+	}
 }
