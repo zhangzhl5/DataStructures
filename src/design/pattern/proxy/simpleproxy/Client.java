671822c79a9dcde47b90d7e1f5ba8b8dbea5bfea
zhangzhl <185378697@qq.com>
2020-04-26 23:13:58 +0800

    动态代理
    Change-Id: I221f84d4a77d32995d24382e64e8dc8a00065be0
    Signed-off-by: zhangzhl <185378697@qq.com>

---
+++ src/design/pattern/proxy/simpleproxy/Client.java
@@ -0,0 +1,18 @@
+package design.pattern.proxy.simpleproxy;
+
+
+/**
+ * 
+ * @author zhangzhl
+ *
+ */
+public class Client {
+
+    public static void main(String[] args) {
+
+        Proxy proxy = new Proxy(new RealSubject());
+        proxy.request();
+
+    }
+
+}
---
+++ src/design/pattern/proxy/simpleproxy/RealSubject.java
@@ -0,0 +1,8 @@
+package design.pattern.proxy.simpleproxy;
+
+public class RealSubject implements Subject{
+    @Override
+    public void request() {
+        System.out.println("real service is called.");
+    }
+}
